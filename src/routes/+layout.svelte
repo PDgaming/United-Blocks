84dc06b972baf6b488dc507abdbcdf4e8964c7cf
PDgaming <produnyadehingia12345@gmail.com>
2026-01-21 16:47:54 +0530

    env issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { setupConvex } from 'convex-svelte';
-	import { CONVEX_URL } from '$env/static/public';
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 
 	let { children } = $props();
-	setupConvex(CONVEX_URL);
+	setupConvex(PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
