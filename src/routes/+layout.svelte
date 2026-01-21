69e23b84ac59e21fc9cd596887d646ba39091d89
PDgaming <produnyadehingia12345@gmail.com>
2026-01-21 16:36:15 +0530

    build fail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { setupConvex } from 'convex-svelte';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { CONVEX_URL } from '$env/static/public';
 
 	let { children } = $props();
-	setupConvex(PUBLIC_CONVEX_URL);
+	setupConvex(CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
